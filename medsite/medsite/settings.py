5f5f8e840e4f8c0e3feb77b9a7746530a35ecc66
Sofia Bilous <EgikSonya@gmail.com>
2023-10-01 14:46:50 +0300

    docker, gunicorn

--- medsite/medsite/settings.py
+++ medsite/medsite/settings.py
@@ -108,13 +108,13 @@ WSGI_APPLICATION = 'medsite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("PG_DB"),
+        "USER": config("PG_USER"),
+        "PASSWORD": config("PG_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     }
 }
 
