24c36adf4253cfc40110f9d2d8d9b5552d1881b9
Sofia Bilous <EgikSonya@gmail.com>
2023-10-05 16:13:21 +0300

    run npm lint:fix

--- medsite/FrontEnd/.eslintrc.js
+++ medsite/FrontEnd/.eslintrc.js
@@ -1,52 +1,52 @@
 module.exports = {
-  "env": {
-    "node": true,
-    "jest": true,
-    "browser": true,
-    "es2021": true
+  'env': {
+    'node': true,
+    'jest': true,
+    'browser': true,
+    'es2021': true
   },
-  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:react-hooks/recommended"],
-  "parserOptions": {
-    "ecmaVersion": 12,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
+  'extends': ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'jsx': true
     }
   },
-  "plugins": ["react", "react-hooks"],
-  "rules": {
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "warn",
-    "max-len": [
-      "warn",
+  'plugins': ['react', 'react-hooks'],
+  'rules': {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'warn',
+    'max-len': [
+      'warn',
       {
-        "code": 120,
-        "ignoreTemplateLiterals": true
+        'code': 120,
+        'ignoreTemplateLiterals': true
       }
     ],
-    "linebreak-style": [
-      "error",
-      "unix"
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    "quotes": [
-      "error",
-      "single"
+    'quotes': [
+      'error',
+      'single'
     ],
-    "semi": [
-      "error",
-      "always"
+    'semi': [
+      'error',
+      'always'
     ],
-    "no-console": [
-      "warn",
+    'no-console': [
+      'warn',
       {
-        "allow": [
-          "warn",
-          "error",
-          "info"
+        'allow': [
+          'warn',
+          'error',
+          'info'
         ]
       }
     ],
-    "no-trailing-spaces": 2,
-    "jsx-quotes": ["error", "prefer-double"]
+    'no-trailing-spaces': 2,
+    'jsx-quotes': ['error', 'prefer-double']
   }
-}
+};
