e6e841edc243f45d5023c78899e6eeb4e8f13183
Sofia Bilous <EgikSonya@gmail.com>
2023-10-05 15:44:15 +0300

    scripts rules

--- medsite/FrontEnd/.eslintrc.js
+++ medsite/FrontEnd/.eslintrc.js
@@ -1,98 +1,52 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+{
+  "env": {
+    "node": true,
+    "jest": true,
+    "browser": true,
+    "es2021": true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:storybook/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-    project: './tsconfig.json',
+  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:react-hooks/recommended"],
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'functional',
-    'import',
-    'no-null',
-    'rxjs',
-  ],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  rules: {
-    // General
-    'no-console': ['error', { allow: ['debug', 'warn', 'error'] }],
-    'react/jsx-boolean-value': 'warn',
-    'react/jsx-curly-brace-presence': 'warn',
-    'react/jsx-fragments': 'warn',
-    'react/jsx-no-useless-fragment': 'warn',
-    'react/jsx-uses-react': 'off',
-    'react/prefer-stateless-function': 'warn',
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    // Functional
-    'functional/prefer-readonly-type': [
-      'error',
+  "plugins": ["react", "react-hooks"],
+  "rules": {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "warn",
+    "max-len": [
+      "warn",
       {
-        allowLocalMutation: true,
-        allowMutableReturnType: true,
-        ignoreClass: true,
-      },
+        "code": 120,
+        "ignoreTemplateLiterals": true
+      }
     ],
-    'import/order': [
-      'error',
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-console": [
+      "warn",
       {
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          ['parent', 'sibling'],
-          'object',
-          'type',
-          'index',
-        ],
-        pathGroups: [
-          {
-            pattern: '{react,react-dom/**}',
-            group: 'external',
-            position: 'before',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['react'],
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-      },
+        "allow": [
+          "warn",
+          "error",
+          "info"
+        ]
+      }
     ],
-    'sort-imports': [
-      'error',
-      {
-        ignoreCase: true,
-        ignoreDeclarationSort: true,
-        ignoreMemberSort: false,
-        allowSeparatedGroups: true,
-      },
-    ],
-    'linebreak-style': ['error', 'unix'],
-    eqeqeq: ['error', 'always', { null: 'ignore' }],
-    camelcase: ['error', { properties: 'never', allow: ['_UNSTABLE'] }],
-    quotes: ['error', 'single', { avoidEscape: true }],
-    'no-null/no-null': 2,
-    curly: ['error', 'multi-line', 'consistent'],
-    'nonblock-statement-body-position': ['error', 'beside'],
-  },
-};
+    "no-trailing-spaces": 2,
+    "jsx-quotes": ["error", "prefer-double"]
+  }
+}
