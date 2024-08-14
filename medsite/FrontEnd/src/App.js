19e1198239535cfefd846c572cdde2685123992b
Sofia Bilous <EgikSonya@gmail.com>
2024-08-14 16:49:20 +0300

    separate frontend container

--- medsite/FrontEnd/src/App.js
+++ medsite/FrontEnd/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 //import Person from './components/Person';
 //import persons from './data/persons';
-import Persons from './components/Persons';
-import cookieconsent from './CookieConsent';
+//import Persons from './components/Persons';
+//import cookieconsent from './CookieConsent';
 
 function App() {
   return (
     <div className="App">
-      <Persons />
-      {cookieconsent}
     </div>
   );
 }
