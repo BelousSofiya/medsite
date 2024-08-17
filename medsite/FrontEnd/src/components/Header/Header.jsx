ee63748ec2f4cebfb35e4e4a82f3ed45ffcc3841
Sofia Bilous <EgikSonya@gmail.com>
2024-08-17 17:43:02 +0300

    side menu

--- medsite/FrontEnd/src/components/Header/Header.jsx
+++ medsite/FrontEnd/src/components/Header/Header.jsx
@@ -4,11 +4,11 @@ import styles from './Header.module.css';
 export default function Header() {
   return (
     <div className={styles['main-header']}>
-      <div className={styles['med-site']}>MEDsite</div>
-      <div className={styles['log-reg-buttons']}>
-        <div className={styles['button']}>login</div>
-        <div className={styles['button']}>register</div>
-      </div>
+      <div className={styles['logo']}>MEDsite</div>
+      <nav className={styles['log-reg-buttons']}>
+        <div>login</div>
+        <div>register</div>
+      </nav>
     </div>
   );
 }
---
+++ medsite/FrontEnd/src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import Header from '../Header/Header';
+import MainMenu from '../Header/MainMenu';
+import SideMenu from '../Header/SideMenu';
+
+export default function Main() {
+  return (
+    <div>
+      <Header />
+      <MainMenu />
+      <SideMenu />
+    </div>
+  );
+}
